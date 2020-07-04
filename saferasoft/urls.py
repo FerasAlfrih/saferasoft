39d5cc1154f572a590435dc35857e961d1bebb81
Feras <alfrih.feras@outlook.com>
2020-07-05 00:14:21 +0300

    static working

--- saferasoft/urls.py
+++ saferasoft/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views as mv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mv.base, name="base"),
+    path('about/', mv.about, name="about"),
 ]
