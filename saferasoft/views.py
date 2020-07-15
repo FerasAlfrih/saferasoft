604a2a467606f324d49bf4a2f520b8ad6c9fdfde
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-07-15 12:44:47 +0300

    Update views.py

--- saferasoft/views.py
+++ saferasoft/views.py
@@ -6,44 +6,26 @@ import re
 
 
 def about(request):
-    lang = get_language_from_request(request)
+   # lang = get_language_from_request(request)
 
-    if lang == 'ar':
-        return render(request, 'saferasoft/ar/about.html')
-    else:
-        return render(request, 'saferasoft/about.html')
+   # if lang == 'ar':
+   #     return render(request, 'saferasoft/ar/about.html')
+   # else:
+    #    return render(request, 'saferasoft/about.html')
 
+return render(request, 'soon.html')
 
 def profolio(request):
-    lang = get_language_from_request(request)
-    if lang == 'ar':
-        return render(request, 'saferasoft/ar/profolio.html')
-    else:
-        return render(request, 'saferasoft/profolio.html')
-
+    return render(request, 'soon.html')
 
 def contact(request):
-    lang = get_language_from_request(request)
-    if lang == 'ar':
-        return render(request, 'saferasoft/ar/contact.html')
-    else:
-        return render(request, 'saferasoft/contact.html')
-
+    return render(request, 'soon.html')
 
 def services(request):
-    lang = get_language_from_request(request)
-    if lang == 'ar':
-        return render(request, 'saferasoft/ar/services.html')
-    else:
-        return render(request, 'saferasoft/services.html')
-
+    return render(request, 'soon.html')
 
 def team(request):
-    lang = get_language_from_request(request)
-    if lang == 'ar':
-        return render(request, 'saferasoft/ar/team.html')
-    else:
-        return render(request, 'saferasoft/team.html')
+    return render(request, 'soon.html')
 
 def is_mobile(request):
 
